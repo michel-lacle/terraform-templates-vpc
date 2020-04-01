cc25232af15d37fe3f78c143471cf05ace84624e
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-31 21:51:01 -0400

    simplify vpc name

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -1,8 +1,10 @@
 resource "aws_vpc" "template-vpc" {
   cidr_block = "10.0.0.0/16"
 
+  assign_generated_ipv6_cidr_block = true
+
   tags = {
-    Name = "terraform-templates-vpc"
+    Name = "templates-vpc"
     Owner = "terraform-templates-vpc"
     Project = "terraform-templates-vpc"
   }
