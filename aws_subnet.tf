efca0be043dbc0d30af3cfff6b055051f64e77b7
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-31 22:02:18 -0400

    add subnets

---
+++ aws_subnet.tf
@@ -0,0 +1,25 @@
+resource "aws_subnet" "template-public" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.template-vpc.id
+
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "template-public"
+    Owner = "terraform-templates-vpc"
+    Project = "terraform-templates-vpc"
+  }
+}
+
+resource "aws_subnet" "template-private" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.template-vpc.id
+
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "template-private"
+    Owner = "terraform-templates-vpc"
+    Project = "terraform-templates-vpc"
+  }
+}
