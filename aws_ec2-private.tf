87d9f37b1c9b5560e84485fac3c677c9a3068739
papiamento <1024836+papiamento@users.noreply.github.com>
2020-04-03 10:44:03 -0400

    add webserver to private subnet

---
+++ aws_ec2-private.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "ec2-sg-webserver" {
+  name = "ec2-public-webserver-sg"
+  description = "Allow ssh & http inbound traffic"
+  vpc_id = aws_vpc.template-vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http traffic"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-private-webserver-sg"
+    Owner = "terraform-templates-vpc"
+    Project = "terraform-templates-vpc"
+  }
+}
+
+resource "aws_instance" "public-ec2-webserver" {
+  ami = "ami-0a887e401f7654935"
+  instance_type = "t2.micro"
+
+  # this is optional, but needed if you want to ssh into your ec2 instance
+  # here I have manually created a key pair in the console and I'm supplying the
+  # name.
+  key_name = "terraform-templates-vpc"
+
+  user_data = file("webserver_install.sh")
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2-sg-webserver.id]
+
+  subnet_id = aws_subnet.template-private.id
+
+  tags = {
+    Name = "private-ec2-webserver"
+    Owner = "terraform-templates-vpc"
+    Project = "terraform-templates-vpc"
+  }
+}
+
