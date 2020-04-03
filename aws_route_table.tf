33e21d4960720656cb08aa6ca2b8fb4aa7dc1907
papiamento <1024836+papiamento@users.noreply.github.com>
2020-04-03 10:16:01 -0400

    fix destination route to ig

--- aws_route_table.tf
+++ aws_route_table.tf
@@ -1,8 +1,12 @@
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.template-vpc.id
 
+  # default routes are created by implicitly, so we don't have to
+  # specify them. example: 10.0.0.0/16(destination) --> local(target)
+
+  # route all external traffic through the internet gateway
   route {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.template-internet-gateway.id
   }
 
